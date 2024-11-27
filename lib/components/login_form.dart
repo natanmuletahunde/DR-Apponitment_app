92793c124a02387f99cb9a81ad394e95db331840
pivnati <natanmuleta911@gmail.com>
2024-11-27 05:30:25 -0700

    create the Textformfield  for both email and password

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -1,4 +1,6 @@
+import 'package:drappointment/utils/config.dart';
 import 'package:flutter/material.dart';
+
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
 
@@ -7,10 +9,61 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  bool obsecurPass = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Login Text'),
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Config.primaryColor,
+            decoration: const InputDecoration(
+              hintText: 'Email Address',
+              alignLabelWithHint: true,
+              prefixIcon: Icon(Icons.email_outlined),
+              prefixIconColor: Config.primaryColor,
+            ),
+          ),
+          Config.spaceSmall,
+          TextFormField(
+            controller: _passController,
+            keyboardType: TextInputType.visiblePassword,
+            cursorColor: Config.primaryColor,
+            obscureText: obsecurPass,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              labelText: 'Password',
+              alignLabelWithHint: true,
+              prefixIcon: const Icon(Icons.lock_outline),
+              prefixIconColor: Config.primaryColor,
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    obsecurPass = !obsecurPass;
+                  });
+                },
+                icon: obsecurPass
+                    ? const Icon(
+                        Icons.visibility_off_outlined,
+                        color: Colors.black38,
+                      )
+                    : const Icon(
+                        Icons.visibility_outlined,
+                        color: Config.primaryColor,
+                      ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
