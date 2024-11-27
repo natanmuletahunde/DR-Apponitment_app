7931a8fb645abe57c2cf346295b9c297e0a63f53
pivnati <natanmuleta911@gmail.com>
2024-11-27 06:17:31 -0700

    create the png og google and facebook .png files in the assets

---
+++ lib/components/social_button.dart
@@ -0,0 +1,31 @@
+import 'package:drappointment/utils/config.dart';
+import 'package:flutter/material.dart';
+
+class SocialButton extends StatelessWidget {
+  const SocialButton({super.key,required this.socail});
+   
+   final String socail;
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: (){}
+      child:SizedBox(
+        width: Config.widthSize * 0.4,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children:<Widget>[
+            Image.asset('assets/$socail.png',
+            width: 40,
+            height: 40,),
+            Text(
+              socail.toUpperCase(),
+              style: const TextStyle(
+                color: Colors.black
+              )
+            )
+          ],
+        ),
+      ) ,
+    );
+  }
+}
