fbfd1f1878748c408f1ee7eb961a02561c9c00ff
pivnati <natanmuleta911@gmail.com>
2024-11-26 18:18:57 -0700

    create the text.dart and  write down all the required   text that are used for the text.dart

---
+++ lib/utils/text.dart
@@ -0,0 +1,14 @@
+
+class AppText {
+  static final enText = {
+       'Welcome_text':'Welcome',
+       'SignIn_text':'Sign In to your account',
+       'registered_text':'Already have an account?',
+       'registere_text':
+             'you can easily sign up,and connect to the Doctor nearby you',
+       'signUp_text':'Don\'t have an account',
+       'social-login':'Or Containe with social account',
+       'forgot-password':'Forgot your password'
+  };
+}
+
