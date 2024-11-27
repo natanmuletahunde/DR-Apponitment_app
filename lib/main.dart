e56ee0afe37a7c34c37c785e8dd30f56dd649e71
pivnati <natanmuleta911@gmail.com>
2024-11-27 10:38:16 -0700

    create the assets outside the lib folder

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// now lets build  and page view 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Doctor App'),
+      ),
+    );
+  }
+}
--- lib/utils/text.dart
+++ lib/utils/text.dart
@@ -1,14 +1,12 @@
-
 class AppText {
   static final enText = {
-       'Welcome_text':'Welcome',
-       'SignIn_text':'Sign In to your account',
-       'registered_text':'Already have an account?',
-       'registere_text':
-       'you can easily sign up,and connect to the Doctor nearby you',
-       'signUp_text':'Don\'t have an account',
-       'socail-login':'Or Containe with social account',
-       'forgot-password':'Forgot your password'
+    'Welcome_text': 'Welcome',
+    'SignIn_text': 'Sign In to your account',
+    'registered_text': 'Already have an account?',
+    'registere_text':
+        'You can easily sign up and connect to the Doctor nearby you',
+    'signUp_text': 'Don\'t have an account',
+    'social-login': 'Or Continue with social account', // Corrected typo here
+    'forgot-password': 'Forgot your password',
   };
 }
-
