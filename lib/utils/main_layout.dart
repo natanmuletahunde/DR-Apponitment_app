a1c055a4f217438b18c26a19a8fe2dc67a2b83bc
pivnati <natanmuleta911@gmail.com>
2024-11-27 19:25:01 -0700

    add the flutter app import 'package:font_awesome_flutter/font_awesome_flutter.dart';

--- lib/utils/main_layout.dart
+++ lib/utils/main_layout.dart
@@ -1,4 +1,8 @@
+import 'package:drappointment/screens/appointment_page.dart';
+import 'package:drappointment/screens/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 class MainLayout extends StatefulWidget {
   const MainLayout({super.key});
@@ -16,7 +20,38 @@ class _MainLayoutState extends State<MainLayout> {
     return Scaffold(
       body: PageView(
         controller: _page,
+        onPageChanged: ((value){
+          setState(() {
+            currentPage=value;
+          });
+        }),
+        children: const <Widget>[
+           HomePage(),
+           AppointmentPage(),
+        ],
       ),
+      bottomNavigationBar: BottomNavigationBar(currentIndex:currentPage,
+      onTap: (page){
+        setState(() {
+          currentPage = page;
+          _page.animateToPage(page, duration: const Duration(
+            milliseconds:500
+          ), curve: Curves.easeInOut);
+          
+        });
+
+      } 
+      ,
+      items: const <BottomNavigationBarItem>[
+  BottomNavigationBarItem(
+    icon: FaIcon(FontAwesomeIcons.houseChimneyMedical),
+    label: 'Home',
+  ),
+  BottomNavigationBarItem(
+    icon: FaIcon(FontAwesomeIcons.solidCalendarCheck),
+    label: 'Appointment',
+  ),
+],),
     );
   }
 }
