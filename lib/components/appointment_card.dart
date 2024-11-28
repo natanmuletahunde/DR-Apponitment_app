7665a7399c12194af53b743b65708a624b63b27d
pivnati <natanmuleta911@gmail.com>
2024-11-27 23:33:27 -0700

    modify the appointment_card.dart

---
+++ lib/components/appointment_card.dart
@@ -0,0 +1,29 @@
+import 'package:drappointment/utils/config.dart';
+import 'package:flutter/material.dart';
+
+class AppointmentCard extends StatefulWidget {
+  const AppointmentCard({super.key});
+
+  @override
+  State<AppointmentCard> createState() => _AppointmentCardState();
+}
+
+class _AppointmentCardState extends State<AppointmentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Config.primaryColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: const Material(
+        color: Colors.transparent,
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(children: <Widget>[]),
+        ),
+      ),
+    );
+  }
+}
