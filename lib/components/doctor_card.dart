216a80f17fee68c09e3e7a0bd883042afbd45b6f
pivnati <natanmuleta911@gmail.com>
2024-11-28 08:51:25 -0700

    create the container and gesturedetector in the child

--- lib/components/doctor_card.dart
+++ lib/components/doctor_card.dart
@@ -1,1 +1,29 @@
 import 'package:flutter/material.dart';
+
+class DoctorCard extends StatefulWidget {
+  const DoctorCard({super.key});
+
+  @override
+  State<DoctorCard> createState() => _DoctorCardState();
+}
+
+class _DoctorCardState extends State<DoctorCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 10, vertical: 10
+      ),
+      height: 150,
+      child: GestureDetector(
+        child: Card(
+          elevation: 5,
+          color: Colors.white,
+          child: Row(
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
