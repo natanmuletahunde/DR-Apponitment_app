5f5e49b7aec2aa9fd4805b8ea2f2d36a1bc98d76
pivnati <natanmuleta911@gmail.com>
2024-11-28 10:15:15 -0700

    create the appointmentPage.dart

--- lib/screens/appointment_page.dart
+++ lib/screens/appointment_page.dart
@@ -7,13 +7,44 @@ class AppointmentPage extends StatefulWidget {
   State<AppointmentPage> createState() => _AppointmentPageState();
 }
 
+enum FilterStatus { upcoming, complete,cancel}
+
 class _AppointmentPageState extends State<AppointmentPage> {
+  FilterStatus status = FilterStatus.upcoming;
+  Alignment _alignment = Alignment.centerLeft;
+ List<Map<String, String>> schedules = [
+  {
+    "doctor_name": "Richard Tan",
+    "doctor_profile": "assets/doctor2.jpg",
+    "category": "Dental",
+    "status":"upcoming"
+  },
+    {
+    "doctor_name": "Max Lim",
+    "doctor_profile": "assets/doctor3.jpg",
+    "category": "Cardiology",
+    "status":"complete"
+  },
+    {
+    "doctor_name": "Jane Wong",
+    "doctor_profile": "assets/doctor4.jpg",
+    "category": "Respiration",
+    "status":""
+  },
+    {
+    "doctor_name": "Jenny Song",
+    "doctor_profile": "assets/doctor5.jpg",
+    "category": "General",
+    "status":"cancel"
+  },
+
+];
+
   @override
-  Widget build(BuildContext context) {
-    // in this appointment page
-    // there are 3 status  ,upcoming, complete and cancel
-    // so need to create 3 status tabs for filtering Appointments status 
-    return const Center(
+  Widget build(BuildContext context) { 
+    // return  filtered appointment
+    // lets create schedule infom 
+    return const  Center(
       child: Text('Appointment page'),
     );
   }
