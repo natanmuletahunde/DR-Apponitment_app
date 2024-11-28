95c827295e5536481f1f891f4ae79626dcd63315
pivnati <natanmuleta911@gmail.com>
2024-11-27 21:11:46 -0700

    remove the const from the FaIcon

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,35 +10,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Map<String,dynamic>> medCat = [ // dynamic un this case means the value can be any type.
-     {
-      "icon":FontAwesomeIcons.userDoctor,
-      "category":"General",
-     },
-      {
-      "icon":FontAwesomeIcons.heartPulse,
-      "category":"Cardiology",
-     },
-      {
-      "icon":FontAwesomeIcons.lungs,
-      "category":"Respirations",
-     },
-     {
-      "icon":FontAwesomeIcons.hand,
-      "category":"Dermatology",
-     }, 
-     {
-      "icon":FontAwesomeIcons.personPregnant,
-      "category":"Gynecology",
-     }, 
-     {
-      "icon":FontAwesomeIcons.teeth,
-      "category":"Dental",
-     }, 
+  List<Map<String, dynamic>> medCat = [
+    // dynamic un this case means the value can be any type.
+    {
+      "icon": FontAwesomeIcons.userDoctor,
+      "category": "General",
+    },
+    {
+      "icon": FontAwesomeIcons.heartPulse,
+      "category": "Cardiology",
+    },
+    {
+      "icon": FontAwesomeIcons.lungs,
+      "category": "Respirations",
+    },
+    {
+      "icon": FontAwesomeIcons.hand,
+      "category": "Dermatology",
+    },
+    {
+      "icon": FontAwesomeIcons.personPregnant,
+      "category": "Gynecology",
+    },
+    {
+      "icon": FontAwesomeIcons.teeth,
+      "category": "Dental",
+    },
   ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 15,
@@ -49,7 +50,7 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Row(
+          const Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text(
@@ -68,26 +69,40 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
           Config.spaceSmall,
-             // category listing
-             Text(
-                'Category',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-               ),
-              ),
-      Config.spaceSmall,
-      SizedBox(
-       height: Config.heightSize * 0.05,
-       child: ListView(
-        scrollDirection: Axis.horizontal,
-        children:List<Widget>.generate(medCat.length, (index{
-          return Card(
-            
-          )
-        })),
-       ),
-      )
+          // category listing
+          const Text(
+            'Category',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Config.spaceSmall,
+          SizedBox(
+            height: Config.heightSize! * 0.05,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List<Widget>.generate(medCat.length, (index) {
+                return Card(
+                  margin: const EdgeInsets.only(right: 20),
+                  color: Config.primaryColor,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        FaIcon(
+                          medCat[index]['icon'],
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
         ],
       )),
     ));
