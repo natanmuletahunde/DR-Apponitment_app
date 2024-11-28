111d89cbfd42cb287085ea1985918c61ce6feb9e
pivnati <natanmuleta911@gmail.com>
2024-11-28 13:48:11 -0700

    modfiy the appointment_page.dart

--- lib/screens/appointment_page.dart
+++ lib/screens/appointment_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:drappointment/utils/config.dart';
 
 class AppointmentPage extends StatefulWidget {
   const AppointmentPage({super.key});
@@ -12,30 +13,31 @@ enum FilterStatus { upcoming, complete, cancel }
 class _AppointmentPageState extends State<AppointmentPage> {
   FilterStatus status = FilterStatus.upcoming;
   Alignment _alignment = Alignment.centerLeft;
+
   List<Map<String, String>> schedules = [
     {
       "doctor_name": "Richard Tan",
       "doctor_profile": "assets/doctor2.jpg",
       "category": "Dental",
-      "status": "upcoming"
+      "status": "upcoming",
     },
     {
       "doctor_name": "Max Lim",
       "doctor_profile": "assets/doctor3.jpg",
       "category": "Cardiology",
-      "status": "complete"
+      "status": "complete",
     },
     {
       "doctor_name": "Jane Wong",
       "doctor_profile": "assets/doctor4.jpg",
       "category": "Respiration",
-      "status": ""
+      "status": "complete",
     },
     {
       "doctor_name": "Jenny Song",
       "doctor_profile": "assets/doctor5.jpg",
       "category": "General",
-      "status": "cancel"
+      "status": "cancel",
     },
   ];
 
@@ -43,34 +45,27 @@ class _AppointmentPageState extends State<AppointmentPage> {
   Widget build(BuildContext context) {
     // Filtered appointment schedules
     List<Map<String, String>> filteredSchedules = schedules.where((schedule) {
-      switch (schedule['status']) {
-        case 'upcoming':
-          return status == FilterStatus.upcoming;
-        case 'complete':
-          return status == FilterStatus.complete;
-        case 'cancel':
-          return status == FilterStatus.cancel;
-        default:
-          return false;
-      }
+      return schedule['status'] == status.name;
     }).toList();
 
-    return const SafeArea(child: 
-    Padding(padding: EdgeInsets.only(left: 20,top: 20,right: 20),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const Text(
-          'Appointment Schedule',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-        )
-      ],
-    ),
-    ),
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Appointment Schedule',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Config.spaceSmall,
+          ],
+        ),
+      ),
     );
   }
 }
