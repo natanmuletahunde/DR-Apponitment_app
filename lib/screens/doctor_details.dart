7ccde6063494affd1b9af0c75ba064ae344cc69a
pivnati <natanmuleta911@gmail.com>
2024-11-29 07:36:27 -0700

    modfiy the doctor_details

--- lib/screens/doctor_details.dart
+++ lib/screens/doctor_details.dart
@@ -1,3 +1,4 @@
+import 'package:drappointment/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -41,6 +42,17 @@ class _DoctorDetailsState extends State<DoctorDetails> {
         ],
       )),
         
+    );
+  }
+}
+
+class AboutDoctor extends StatelessWidget {
+  const AboutDoctor({super.key});
+  @override
+  Widget build(BuildContext context) {
+    Config.init(context);
+    return Container(
+      width: double.infinity,
     );
   }
 }
