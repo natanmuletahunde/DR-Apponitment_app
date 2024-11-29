92f07ceb959da0c4be67956931d51473cbd31947
pivnati <natanmuleta911@gmail.com>
2024-11-29 10:52:52 -0700

    ' finshed login page

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -17,62 +17,70 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            controller: _emailController,
-            keyboardType: TextInputType.emailAddress,
-            cursorColor: Config.primaryColor,
-            decoration: const InputDecoration(
-              hintText: 'Email Address',
-              alignLabelWithHint: true,
-              prefixIcon: Icon(Icons.email_outlined),
-              prefixIconColor: Config.primaryColor,
+    return Scaffold( // Make sure this is within Scaffold
+      resizeToAvoidBottomInset: true, // Ensures the body adjusts when keyboard is shown
+      body: SingleChildScrollView( // Wrap the entire body in SingleChildScrollView
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  cursorColor: Config.primaryColor,
+                  decoration: const InputDecoration(
+                    hintText: 'Email Address',
+                    alignLabelWithHint: true,
+                    prefixIcon: Icon(Icons.email_outlined),
+                    prefixIconColor: Config.primaryColor,
+                  ),
+                ),
+                Config.spaceSmall,
+                TextFormField(
+                  controller: _passController,
+                  keyboardType: TextInputType.visiblePassword,
+                  cursorColor: Config.primaryColor,
+                  obscureText: obsecurPass,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    alignLabelWithHint: true,
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    prefixIconColor: Config.primaryColor,
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          obsecurPass = !obsecurPass;
+                        });
+                      },
+                      icon: obsecurPass
+                          ? const Icon(
+                              Icons.visibility_off_outlined,
+                              color: Colors.black38,
+                            )
+                          : const Icon(
+                              Icons.visibility_outlined,
+                              color: Config.primaryColor,
+                            ),
+                    ),
+                  ),
+                ),
+                Config.spaceSmall,
+                Button(
+                    title: 'Sign In ',
+                    width: double.infinity,
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('main');
+                    },
+                    disable: false)
+                // login button
+              ],
             ),
           ),
-          Config.spaceSmall,
-          TextFormField(
-            controller: _passController,
-            keyboardType: TextInputType.visiblePassword,
-            cursorColor: Config.primaryColor,
-            obscureText: obsecurPass,
-            decoration: InputDecoration(
-              hintText: 'Password',
-              labelText: 'Password',
-              alignLabelWithHint: true,
-              prefixIcon: const Icon(Icons.lock_outline),
-              prefixIconColor: Config.primaryColor,
-              suffixIcon: IconButton(
-                onPressed: () {
-                  setState(() {
-                    obsecurPass = !obsecurPass;
-                  });
-                },
-                icon: obsecurPass
-                    ? const Icon(
-                        Icons.visibility_off_outlined,
-                        color: Colors.black38,
-                      )
-                    : const Icon(
-                        Icons.visibility_outlined,
-                        color: Config.primaryColor,
-                      ),
-              ),
-            ),
-          ),
-          Config.spaceSmall,
-          Button(
-              title: 'Sign In ',
-              width: double.infinity,
-              onPressed: () {
-               Navigator.of(context).pushNamed('main'); 
-              },
-              disable: false)
-          //  login button
-        ],
+        ),
       ),
     );
   }
--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -19,131 +19,129 @@ class _AuthPageState extends State<AuthPage> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
         child: SafeArea(
-          child: SingleChildScrollView(  // Wrap the Column in SingleChildScrollView to fix overflow
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  AppText.enText['Welcome_text']!,
-                  style: const TextStyle(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                  ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                AppText.enText['Welcome_text']!,
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  AppText.enText['SignIn_text']!,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+              ),
+              Text(
+                AppText.enText['SignIn_text']!,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-                Config.spaceSmall,
-                LoginForm(),
-                Config.spaceSmall,
-                Center(
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(AppText.enText['forgot-password']!,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        )),
-                  ),
-                ),
-                // add social button sign in
-                const Spacer(),
-                Center(
-                  child: Text(
-                    AppText.enText['social-login']!,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Config.spaceSmall,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Action for Google button
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                Config.primaryColor, // Button background color
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 12, horizontal: 16),
-                          ),
-                          child: const Text(
-                            'Google',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white, // White text color
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Flexible(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            // Action for Facebook button
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                Config.primaryColor, // Button background color
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 12, horizontal: 16),
-                          ),
-                          child: const Text(
-                            'Facebook',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white, // White text color
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Config.spaceSmall,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      AppText.enText['signUp_text']!,
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey.shade500,
-                      ),
-                    ),
-                    const Padding(padding: EdgeInsets.only(
-                      right:5
-                    )),
-                     const Text(
-                      'Sign Up',
+              ),
+              Config.spaceSmall,
+              LoginForm(),
+              Config.spaceSmall,
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(AppText.enText['forgot-password']!,
                       style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
-                        color: Colors.black
+                        color: Colors.black,
+                      )),
+                ),
+              ),
+              // add social button sign in
+              const Spacer(),
+              Center(
+                child: Text(
+                  AppText.enText['social-login']!,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Action for Google button
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Config.primaryColor, // Button background color
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 16),
+                        ),
+                        child: const Text(
+                          'Google',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // White text color
+                          ),
+                        ),
                       ),
-                    )
-                  ],
-                )
-              ],
-            ),
+                    ),
+                  ),
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Action for Facebook button
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Config.primaryColor, // Button background color
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 16),
+                        ),
+                        child: const Text(
+                          'Facebook',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // White text color
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    AppText.enText['signUp_text']!,
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.only(
+                    right:5
+                  )),
+                   const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                    ),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
