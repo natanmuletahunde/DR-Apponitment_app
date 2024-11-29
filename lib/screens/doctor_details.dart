112af30bd5c87cae95c76d79c96f9b5f2d4abb3d
pivnati <natanmuleta911@gmail.com>
2024-11-28 23:00:23 -0700

    create the custom_appbar.dart

---
+++ lib/screens/doctor_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class DoctorDetails extends StatefulWidget {
+  const DoctorDetails({super.key});
+  @override
+  State<DoctorDetails> createState() => _DoctorDetailsState();
+}
+
+class _DoctorDetailsState extends State<DoctorDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar:// create a custom app bar,
+    );
+  }
+}
