c89bed37930ba3b25fb180f1adae0241591e0ccc
pivnati <natanmuleta911@gmail.com>
2024-11-30 12:54:37 -0700

    create the success.json in the images folder

--- lib/screens/success.booked.dart
+++ lib/screens/success.booked.dart
@@ -1,3 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
+class AppointmentBooked extends StatelessWidget {
+  const AppointmentBooked({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: SafeArea(
+      child: 
+       Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children:<Widget>[
+          Expanded(child: 
+          Lottie(composition: composition)
+          )
+         ],
+       )
+     
+     ),
+      
+    );
+  }
+}
