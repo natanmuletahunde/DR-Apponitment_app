0025de46ee4825d3054faaf7f4d71eb626ebd827
pivnati <natanmuleta911@gmail.com>
2024-11-29 21:54:22 -0700

    create the preferredsize for the customAppbar

--- lib/screens/booking_page.dart
+++ lib/screens/booking_page.dart
@@ -1,7 +1,8 @@
+import 'package:drappointment/components/custom_appbar.dart';
+import 'package:drappointment/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:table_calendar/table_calendar.dart';
-
 
 class BookingPage extends StatefulWidget {
   const BookingPage({super.key});
@@ -11,15 +12,27 @@ class BookingPage extends StatefulWidget {
 }
 
 class _BookingPageState extends State<BookingPage> {
- CalendarFormat _format = CalendarFormat.month;
- DateTime _focusDay = DateTime.now();
- int? _currentIndex ;
- bool _isWeekend = false;
- bool _dateSelected = false;
- bool _timeSelected = false;
-  // declaration 
+  CalendarFormat _format = CalendarFormat.month;
+  DateTime _focusDay = DateTime.now();
+  int? _currentIndex;
+  bool _isWeekend = false;
+  bool _dateSelected = false;
+  bool _timeSelected = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Config.init(context);
+    return const Scaffold(
+      appBar: PreferredSize(
+        preferredSize:  Size.fromHeight(kToolbarHeight),
+        child:  CustomAppBar(
+          appTitle: '',
+          appTile: 'Appointments',
+          icon: FaIcon(Icons.arrow_back_ios),
+          
+        ),
+      ),
+    
+    );
   }
 }
