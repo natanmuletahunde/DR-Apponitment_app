257d0c38a47f1c4ac2263c535b0e7829ba5f7dba
pivnati <natanmuleta911@gmail.com>
2024-12-06 20:38:15 -0700

    make the remove the spell error in the dio_provider.dart

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -18,75 +18,77 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            controller: _emailController,
-            keyboardType: TextInputType.emailAddress,
-            cursorColor: Config.primaryColor,
-            decoration: const InputDecoration(
-              hintText: 'Email Address',
-              alignLabelWithHint: true,
-              prefixIcon: Icon(Icons.email_outlined),
-              prefixIconColor: Config.primaryColor,
-            ),
-          ),
-          Config.spaceSmall,
-          TextFormField(
-            controller: _passController,
-            keyboardType: TextInputType.visiblePassword,
-            cursorColor: Config.primaryColor,
-            obscureText: obsecurPass,
-            decoration: InputDecoration(
-              hintText: 'Password',
-              labelText: 'Password',
-              alignLabelWithHint: true,
-              prefixIcon: const Icon(Icons.lock_outline),
-              prefixIconColor: Config.primaryColor,
-              suffixIcon: IconButton(
-                onPressed: () {
-                  setState(() {
-                    obsecurPass = !obsecurPass;
-                  });
-                },
-                icon: obsecurPass
-                    ? const Icon(
-                        Icons.visibility_off_outlined,
-                        color: Colors.black38,
-                      )
-                    : const Icon(
-                        Icons.visibility_outlined,
-                        color: Config.primaryColor,
-                      ),
+    return SingleChildScrollView( // Added SingleChildScrollView
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              cursorColor: Config.primaryColor,
+              decoration: const InputDecoration(
+                hintText: 'Email Address',
+                alignLabelWithHint: true,
+                prefixIcon: Icon(Icons.email_outlined),
+                prefixIconColor: Config.primaryColor,
               ),
             ),
-          ),
-          Config.spaceSmall,
-          Button(
-            title: 'Sign In',
-            width: double.infinity,
-            onPressed: () async {
-              try {
-                final token = await DioProvider()
-                    .getToken(_emailController.text, _passController.text);
+            Config.spaceSmall,
+            TextFormField(
+              controller: _passController,
+              keyboardType: TextInputType.visiblePassword,
+              cursorColor: Config.primaryColor,
+              obscureText: obsecurPass,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                labelText: 'Password',
+                alignLabelWithHint: true,
+                prefixIcon: const Icon(Icons.lock_outline),
+                prefixIconColor: Config.primaryColor,
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      obsecurPass = !obsecurPass;
+                    });
+                  },
+                  icon: obsecurPass
+                      ? const Icon(
+                          Icons.visibility_off_outlined,
+                          color: Colors.black38,
+                        )
+                      : const Icon(
+                          Icons.visibility_outlined,
+                          color: Config.primaryColor,
+                        ),
+                ),
+              ),
+            ),
+            Config.spaceSmall,
+            Button(
+              title: 'Sign In',
+              width: double.infinity,
+              onPressed: () async {
+                try {
+                  final token = await DioProvider()
+                      .getToken(_emailController.text, _passController.text);
 
-                if (token is String) {
-                  final user = await DioProvider().getUser(token);
-                  print(user);
-                } else {
-                  print("Error: Token is not a string.");
+                  if (token is String) {
+                    final user = await DioProvider().getUser(token);
+                    print(user);
+                  } else {
+                    print("Error: Token is not a string.");
+                  }
+                } catch (e) {
+                  print("Error: $e");
                 }
-              } catch (e) {
-                print("Error: $e");
-              }
-            },
-            disable: false,
-            buttonColor: Colors.green,
-          )
-        ],
+              },
+              disable: false,
+              buttonColor: Colors.green,
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/providers/dio_provider.dart
+++ lib/providers/dio_provider.dart
@@ -6,7 +6,7 @@ class DioProvider {
   Future<dynamic> getToken(String email, String password) async {
     try {
       var response = await Dio().post(
-        'http://192.168.137.246:8000/api/login', // Update with your IP
+        'http://127.0.0.1:8000/api/login', // Update with your IP
         data: {'email': email, 'password': password},
       );
 
@@ -18,12 +18,10 @@ class DioProvider {
       return error;
     }
   }
-
-  // Get user data
   Future<dynamic> getUser(String token) async {
     try {
       var user = await Dio().get(
-        'http://192.168.137.246:8000/api/user',
+        'http://127.0.0.1:8000/api/user',
         options: Options(headers: {'Authorization':'Bearer $token'}) // Update with your IP
       );
       if (user.statusCode == 200 && user.data != null) {
