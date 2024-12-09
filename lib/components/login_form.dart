96bdcfd33b203febcf1df65a178e39c48a95e60b
pivnati <natanmuleta911@gmail.com>
2024-12-08 20:28:39 -0700

    modify the auth_page and login_page

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:drappointment/components/button.dart';
 import 'package:drappointment/providers/dio_provider.dart';
+import 'package:drappointment/screens/home_page.dart';
 import 'package:drappointment/utils/config.dart';
+import 'package:flutter/material.dart';
 
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
@@ -24,76 +25,66 @@ class _LoginFormState extends State<LoginForm> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            _buildEmailField(),
+            TextFormField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              cursorColor: Config.primaryColor,
+              decoration: const InputDecoration(
+                hintText: 'Email Address',
+                alignLabelWithHint: true,
+                prefixIcon: Icon(Icons.email_outlined),
+                prefixIconColor: Config.primaryColor,
+              ),
+            ),
             Config.spaceSmall,
-            _buildPasswordField(),
+            TextFormField(
+              controller: _passController,
+              keyboardType: TextInputType.visiblePassword,
+              cursorColor: Config.primaryColor,
+              obscureText: obsecurPass,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                labelText: 'Password',
+                alignLabelWithHint: true,
+                prefixIcon: const Icon(Icons.lock_outline),
+                prefixIconColor: Config.primaryColor,
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      obsecurPass = !obsecurPass;
+                    });
+                  },
+                  icon: obsecurPass
+                      ? const Icon(
+                          Icons.visibility_off_outlined,
+                          color: Colors.black38,
+                        )
+                      : const Icon(
+                          Icons.visibility_outlined,
+                          color: Config.primaryColor,
+                        ),
+                ),
+              ),
+            ),
             Config.spaceSmall,
-            _buildSignInButton(),
+            Button(
+              title: 'Sign In',
+              width: double.infinity,
+              onPressed: () async {
+                // Attempt to get the token
+                final token = await DioProvider().getToken(
+                  _emailController.text,
+                  _passController.text,
+                );
+               print(token);
+                
+              },
+              disable: false,
+              buttonColor: Colors.green,
+            ),
           ],
         ),
       ),
-    );
-  }
-
-  Widget _buildEmailField() {
-    return TextFormField(
-      controller: _emailController,
-      keyboardType: TextInputType.emailAddress,
-      cursorColor: Config.primaryColor,
-      decoration: const InputDecoration(
-        hintText: 'Email Address',
-        alignLabelWithHint: true,
-        prefixIcon: Icon(Icons.email_outlined),
-        prefixIconColor: Config.primaryColor,
-      ),
-    );
-  }
-
-  Widget _buildPasswordField() {
-    return TextFormField(
-      controller: _passController,
-      keyboardType: TextInputType.visiblePassword,
-      cursorColor: Config.primaryColor,
-      obscureText: obsecurPass,
-      decoration: InputDecoration(
-        hintText: 'Password',
-        labelText: 'Password',
-        alignLabelWithHint: true,
-        prefixIcon: const Icon(Icons.lock_outline),
-        prefixIconColor: Config.primaryColor,
-        suffixIcon: IconButton(
-          onPressed: () {
-            setState(() {
-              obsecurPass = !obsecurPass;
-            });
-          },
-          icon: obsecurPass
-              ? const Icon(
-                  Icons.visibility_off_outlined,
-                  color: Colors.black38,
-                )
-              : const Icon(
-                  Icons.visibility_outlined,
-                  color: Config.primaryColor,
-                ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSignInButton() {
-    return Button(
-      title: 'Sign In',
-      width: double.infinity,
-      onPressed: () async {
-        final token = await DioProvider().getToken(
-          _emailController.text,
-          _passController.text,
-        );
-        print(token);
-      },
-      disable: false,
-      buttonColor: Colors.green,
     );
   }
 }
--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:drappointment/components/login_form.dart';
 import 'package:drappointment/utils/config.dart';
 import 'package:drappointment/utils/text.dart';
+import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
@@ -14,140 +14,137 @@ class _AuthPageState extends State<AuthPage> {
   @override
   Widget build(BuildContext context) {
     Config.init(context);
-
+    // build login page
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                _buildHeaderText(),
-                Config.spaceSmall,
-                const LoginForm(),
-                Config.spaceSmall,
-                _buildForgotPasswordButton(),
-                const Spacer(),
-                _buildSocialLoginText(),
-                Config.spaceSmall,
-                _buildSocialButtons(),
-                Config.spaceSmall,
-                _buildSignUpRow(),
-              ],
-            ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                AppText.enText['Welcome_text']!,
+                style: const TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                AppText.enText['SignIn_text']!,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Config.spaceSmall,
+              LoginForm(),
+              Config.spaceSmall,
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(AppText.enText['forgot-password']!,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      )),
+                ),
+              ),
+              // add social button sign in
+              const Spacer(),
+              Center(
+                child: Text(
+                  AppText.enText['social-login']!,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Action for Google button
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Config.primaryColor, // Button background color
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 16),
+                        ),
+                        child: const Text(
+                          'Google',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // White text color
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Action for Facebook button
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              Config.primaryColor, // Button background color
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 16),
+                        ),
+                        child: const Text(
+                          'Facebook',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white, // White text color
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Config.spaceSmall,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    AppText.enText['signUp_text']!,
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.only(
+                    right:5
+                  )),
+                   const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                    ),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
-    );
-  }
-
-  Widget _buildHeaderText() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          AppText.enText['Welcome_text']!,
-          style: const TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        Text(
-          AppText.enText['SignIn_text']!,
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildForgotPasswordButton() {
-    return Center(
-      child: TextButton(
-        onPressed: () {},
-        child: Text(
-          AppText.enText['forgot-password']!,
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSocialLoginText() {
-    return Center(
-      child: Text(
-        AppText.enText['social-login']!,
-        style: const TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.normal,
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSocialButtons() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        _buildSocialButton('Google', () {}),
-        _buildSocialButton('Facebook', () {}),
-      ],
-    );
-  }
-
-  Widget _buildSocialButton(String title, VoidCallback onPressed) {
-    return Flexible(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: ElevatedButton(
-          onPressed: onPressed,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Config.primaryColor,
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-          ),
-          child: Text(
-            title,
-            style: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSignUpRow() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          AppText.enText['signUp_text']!,
-          style: TextStyle(
-            fontWeight: FontWeight.normal,
-            color: Colors.grey.shade500,
-          ),
-        ),
-        const SizedBox(width: 5),
-        const Text(
-          'Sign Up',
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-      ],
     );
   }
 }
