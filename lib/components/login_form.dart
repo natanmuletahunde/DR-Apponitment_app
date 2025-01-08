ce68bf135246a5ef732f678f51778207b835d397
pivnati <natanmuleta911@gmail.com>
2025-01-08 11:50:50 -0700

    modfiy the home_page and login_page to navgiate to the home screen

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -1,5 +1,4 @@
 import 'package:drappointment/components/button.dart';
-import 'package:drappointment/providers/dio_provider.dart';
 import 'package:drappointment/screens/home_page.dart';
 import 'package:drappointment/utils/config.dart';
 import 'package:flutter/material.dart';
@@ -67,31 +66,19 @@ class _LoginFormState extends State<LoginForm> {
               ),
             ),
             Config.spaceSmall,
-         Button(
-  title: 'Sign In',
-  width: double.infinity,
-  onPressed: () async {
-    // Attempt to get the token
-    final token = await DioProvider().getToken(
-      _emailController.text,
-      _passController.text,
-    );
-
-    final user = await DioProvider().getUser();
-    print(user);
-
-    // Navigate to the HomePage
-    if (token != null && user != null) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomePage()), // Ensure HomePage() is your actual home screen widget
-      );
-    }
-  },
-  disable: false,
-  buttonColor: Colors.green,
-)
-
+            Button(
+              title: 'Sign In',
+              width: double.infinity,
+              onPressed: () {
+                // Navigate directly to the HomePage
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              },
+              disable: false,
+              buttonColor: Colors.green,
+            ),
           ],
         ),
       ),
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,7 +13,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Map<String, dynamic>> medCat = [
-    // dynamic un this case means the value can be any type.
     {
       "icon": FontAwesomeIcons.userDoctor,
       "category": "General",
@@ -39,116 +38,109 @@ class _HomePageState extends State<HomePage> {
       "category": "Dental",
     },
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 15,
-      ),
-      child: SafeArea(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: SafeArea(
           child: SingleChildScrollView(
             child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(
-                  'Amanda',
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Amanda',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage('assets/images/profile1.jpg'),
+                      ),
+                    ),
+                  ],
+                ),
+                Config.spaceSmall,
+                const Text(
+                  'Category',
                   style: TextStyle(
-                    fontSize: 24,
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
+                Config.spaceSmall,
                 SizedBox(
-                  child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: AssetImage('assets/images/profile1.jpg'),
+                  height: Config.heightSize! * 0.05,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List<Widget>.generate(medCat.length, (index) {
+                      return Card(
+                        margin: const EdgeInsets.only(right: 20),
+                        color: Config.primaryColor,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              FaIcon(
+                                medCat[index]['icon'],
+                                color: Colors.white,
+                              ),
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                medCat[index]['category'],
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
                   ),
-                )
+                ),
+                Config.spaceSmall,
+                const Text(
+                  'Appointment Today',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Config.spaceSmall,
+                const AppointmentCard(),
+                Config.spaceSmall,
+                const Text(
+                  'Top Doctor',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Config.spaceSmall,
+                Column(
+                  children: List.generate(10, (index) {
+                    return const DoctorCard(route: 'doc_details');
+                  }),
+                ),
               ],
             ),
-            Config.spaceSmall,
-            // category listing
-            const Text(
-              'Category',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Config.spaceSmall,
-            SizedBox(
-              height: Config.heightSize! * 0.05,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: List<Widget>.generate(medCat.length, (index) {
-                  return Card(
-                    margin: const EdgeInsets.only(right: 20),
-                    color: Config.primaryColor,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          FaIcon(
-                            medCat[index]['icon'],
-                            color: Colors.white,
-                          ),
-                          const SizedBox(
-                            width: 20,
-                          ),
-                          Text(
-                            medCat[index]['category'],
-                            style: const TextStyle(
-                              fontSize: 16,
-                              color:Colors.white
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                }),
-              ),
-            ),
-            Config.spaceSmall,
-            const Text(
-              'Appointment Today',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              )
-              ),
-            Config.spaceSmall,
-             const AppointmentCard(),
-            Config.spaceSmall,
-             const Text(
-              'Top Doctor',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              )
-              ),
-              //List of Top Doctor
-              Config.spaceSmall,
-              Column(
-                children: 
-                  List.generate(10, (index){
-                  return const  DoctorCard(
-                        route:'doc_details',
-                  );
-                  }
-                  )
-                  )
-                    ],
-                  ),
-          )),
-    ));
+          ),
+        ),
+      ),
+    );
   }
 }
